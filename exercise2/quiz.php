6913777ecb0bc60927bf4aa52a3fe2300c972f55
kuebler324 <71736302+kuebler324@users.noreply.github.com>
2022-11-11 14:28:01 -0600

    Add files via upload

--- exercise2/quiz.php
+++ exercise2/quiz.php
@@ -6,19 +6,19 @@
     }
     $q2 = $_POST['q2'];
     if($q2 === 'blue') {
-        $score += 2;
+        $score += 1;
     }
     $q3 = $_POST['q3'];
     if($q3 === 'Them') {
-        $score += 3;
+        $score += 1;
     }
     $q4 = $_POST['q4'];
     if($q4 === 'javascript') {
-        $score += 4;
+        $score += 1;
     }
     $q5 = $_POST['q5'];
     if($q5 === 'Microsoft Teams') {
-        $score += 5;
+        $score += 1;
     }
     $score *= 20;
 ?>
@@ -32,31 +32,31 @@
     <body>
         <div>
             <p>Question 1: How old am I?</p>
-            <p>You answered: <?php=$q1?></p>
+            <p>You answered: <?=$q1?></p>
             <p>Correct answer: a.) 10</p>
         </div>
         <div>
             <p>Question 2: What is my favorite color?</p>
-            <p>You answered: <?php=$q2?></p>
+            <p>You answered: <?=$q2?></p>
             <p>Correct answer: b.) blue</p>
         </div>
         <div>
             <p>Question 3: Who is in charge?</p>
-            <p>You answered: <?php=$q3?></p>
+            <p>You answered: <?=$q3?></p>
             <p>Correct answer: c.) Them</p>
         </div>
         <div>
             <p>Question 4: What is the best language?</p>
-            <p>You answered: <?php=$q4?></p>
+            <p>You answered: <?=$q4?></p>
             <p>Correct answer: d.) javascript</p>
         </div>
         <div>
             <p>Question 5: What is your favorite apple product?</p>
-            <p>You answered: <?php=$q5?></p>
+            <p>You answered: <?=$q5?></p>
             <p>Correct answer: a.) Microsoft Teams</p>
         </div>
         <div>
-            <h1>Your score: <?php=$score?>%</h1>
+            <h1>Your score: <?=$score?>%</h1>
         </div>
     </body>
 </html>
