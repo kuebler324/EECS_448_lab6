57e7dfa3d8c5bfce057f428c2b09988514cfd38d
kuebler324 <71736302+kuebler324@users.noreply.github.com>
2022-11-11 14:05:58 -0600

    Add files via upload

--- exercise1/index.php
+++ exercise1/index.php
@@ -1,1 +1,12 @@
-
+<?php
+    $output = '<table><tbody>';
+    for($y = 0; $y <= 100; ++$y) {
+        $output .= '<tr>';
+        for($x = 0; $x <= 100; ++$x) {
+            $output .= '<td>' . ($y === 0 ? ($x === 0 ? '' : $x) : ($x === 0 ? $y : $x * $y)) . '</td>';
+        }
+        $output .= '</tr>';
+    }
+    $output .= '</tbody></table>';
+    echo $output;
+?>
